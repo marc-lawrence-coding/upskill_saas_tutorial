497cc98785690182396537c698fcf0da87c0eeae
Marc Lawrence <marclawrencestudies@gmail.com>
2019-02-28 09:24:08 +0000

    Added about view, route, controller action

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [Upskill](http://upskillcourses.com) Software-as-a-Service Ruby on Rails App
 
 git push origin HEAD:master
+rails s -b $IP -p $PORT
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
-  def home
-  end
-end   
-
-        
+    def home
+    end
+    
+    def about 
+    end
+end
