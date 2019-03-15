86d5496f060296ced2dc94f2116f6938fe19b203
Marc Lawrence <marclawrencestudies@gmail.com>
2019-03-15 09:42:13 +0000

    improved flash style

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,14 +6,14 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      name = params[:contact][:name]
-      email = params[:contact][:email]
-      body = params[:contact][:comments]
-      ContactMailer.contact_email(name, email, body).deliver
+  name = params[:contact][:name]
+  email = params[:contact][:email]
+  body = params[:contact][:comments]
+  ContactMailer.contact_email(name, email, body).deliver
       flash[:success] ="Message sent."
        redirect_to new_contact_path
     else
-      flash[:error] = @contact.errors.full_messages.join(", ")
+      flash[:danger] = @contact.errors.full_messages.join(", ")
        redirect_to new_contact_path
     end
   end
